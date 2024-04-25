6cc5692bea45756a91e2ce654db5b13e14c06fe1
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-04-25 14:49:31 +0100

    Delete build directory

--- build/js/brawsim.events.js
+++
@@ -1,8 +0,0 @@
-module.exports =  {
-
-    makeValues_changed: function(ui) {
-    let value = ui.makeValues.value();
-    if (value) 
-        ui.sendValues.setValue(!ui.makeValues.value());
-    }
-};
